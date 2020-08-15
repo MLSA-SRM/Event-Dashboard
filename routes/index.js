cf267efa5daca3aed0899b2b0e8ed7c1fd113f64
Aryan <51026686+Violent-Idiot@users.noreply.github.com>
2020-08-15 16:42:00 +0530

    added dbController

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,19 @@
 var express = require("express");
 var router = express.Router();
 
-/* GET home page. */
-router.get("/", (req, res, next) => {
-  res.send("Welcome");
+var events = require("../controller/db");
+
+router.get("/", async (req, res, next) => {
+  let username = "yoman";
+  let data = await events(username);
+  let name = [];
+  //IF YOU WANT TO HAVE DATA FROM ARRAY JUST LOOP IT OVER
+
+  data.events.forEach((item) => {
+    name.push(item.name);
+  });
+  console.log(name);
+  res.json(name);
 });
 
 module.exports = router;
