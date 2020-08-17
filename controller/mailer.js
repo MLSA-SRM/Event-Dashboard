fb3ea26bee836a7ff81d43092e459eb171c92909
Sparsh Srivastava <59307515+Sparsh-Srivastava@users.noreply.github.com>
2020-08-17 19:08:21 +0530

    qrcode in email body

--- controller/mailer.js
+++ controller/mailer.js
@@ -1,12 +1,19 @@
 const Transporter = require("../config/mailConfig");
+var QRCode = require('qrcode');
+var img;
 
 let test = "";
+
+QRCode.toString('https://www.google.co.in/', function (err, url) {
+  console.log(url);
+  img = url;
+});
 
 let mailDetails = {
   from: "yoman@helo.in",
   to: test,
   subject: "Test mail",
-  text: "Nodemailer testing mail, congratulations if you recieved this.",
+  html: "<span><h1 style='color:black'>Welcome</h1><h4>XYZ</h4></span><br><pre>"+ img +"</pre>",
 };
 
 (async () => {
