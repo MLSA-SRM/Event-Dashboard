ca5c3906a1b4abc69e962456b4e0c6bdeb67fc4d
Sparsh Srivastava <59307515+Sparsh-Srivastava@users.noreply.github.com>
2020-09-06 15:01:41 +0530

    Update mailer.js

--- controller/mailer.js
+++ controller/mailer.js
@@ -1,24 +1,39 @@
 const Transporter = require("../config/mailConfig");
 var QRCode = require("qrcode");
-const users = require("./data.json");
+
+const json = [
+  {
+    "name": "John",
+    "reg": "RA001",
+    "email": "abc@gmail.com"
+  },
+  {
+    "name": "Doe",
+    "reg": "RA002",
+    "email": "xyz@gmail.com"
+  }
+];
+
+for( let a in json ){
+    console.log( json[a] );
+
 var img;
 
 let test = "";
 
-QRCode.toDataURL("Yoman", function (err, img) {
-  for(var k in users)
+QRCode.toDataURL(json[a].reg, function (err, img)
 {
-  console.log(users[k].name);
+  console.log(json[a].name);
   let mailDetails = {
     from: "yoman@helo.in",
-    to: users[k].email,
+    to: json[a].email,
     subject: "Test mail",
     attachDataUrls: true,
-    html: `<span style='font-size: 2em;'>Hello</span><span style='color: #ec0101'>${users[k].name}</span><p>Thank You for choosing to attend our event on something. I would be an absolute honour to have you with us. Looking forward to seeing you there. Below you can find detils of your involvement in the event.</p><br><pre><img src="${img}"></pre>`,
+    html: `<span style='font-size: 2em;'>Hello</span><span style='color: #ec0101'>${json[a].name}</span><p>Thank You for choosing to attend our event on something. I would be an absolute honour to have you with us. Looking forward to seeing you there. Below you can find detils of your involvement in the event.</p><br><pre><img src="${img}"></pre>`,
   };
   (async () => {
     let info = await Transporter.sendMail(mailDetails);
     console.log("Message sent: %s", info.messageId);
   })();
-}
 });
+};
