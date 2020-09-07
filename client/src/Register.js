ce35bce8f38f7f51d195b6ef9b50edd72638fe65
jugal09xx <65349424+jugal09xx@users.noreply.github.com>
2020-09-07 09:52:41 +0530

    revert

--- client/src/Register.js
+++ client/src/Register.js
@@ -1,76 +1,77 @@
-import React, { useState } from "react";
-import Axios from "axios";
-import { Link, useHistory } from "react-router-dom";
-import { ToastContainer, toast } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
+import React, { useState } from 'react';
+import Axios from 'axios';
+import { Link, useHistory } from 'react-router-dom';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
-import "./Auth.css";
+import './Auth.css';
 
 function Register() {
-  let history = useHistory();
-  const [registerUsername, setRegisterUsername] = useState("");
-  const [registerPassword, setRegisterPassword] = useState("");
+	let history = useHistory();
+	const [ registerUsername, setRegisterUsername ] = useState('');
+	const [ registerPassword, setRegisterPassword ] = useState('');
 
-  //Toastify Config
-  const notifyFailure = () => toast.error("This username is already taken!",{
-    autoClose: 5000,
-    position: "top-center",
-  });
+	//Toastify Config
+	const notifyFailure = () =>
+		toast.error('This username is already taken!', {
+			autoClose: 5000,
+			position: 'top-center'
+		});
 
-  const registerUser = (e) => {
-    e.preventDefault();
-    Axios({
-      method: "POST",
-      data: {
-        username: registerUsername,
-        password: registerPassword,
-      },
-      withCredentials: true,
-      url: "/register",
-    })
-      .then((res) => {
-        if (res.data) {
-          history.push("/logIn");
-          console.log("Success Sign Up");
-        } else {
-          history.push("/signIn");
-          console.log("This username is already taken");
-          notifyFailure();
-        }
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  };
+	const registerUser = (e) => {
+		e.preventDefault();
+		Axios({
+			method: 'POST',
+			data: {
+				username: registerUsername,
+				password: registerPassword
+			},
+			withCredentials: true,
+			url: '/register'
+		})
+			.then((res) => {
+				if (res.data) {
+					history.push('/logIn');
+					console.log('Success Sign Up');
+				} else {
+					history.push('/signIn');
+					console.log('This username is already taken');
+					notifyFailure();
+				}
+			})
+			.catch((err) => {
+				console.log(err);
+			});
+	};
 
-  return (
-    <div>
-      <ToastContainer />
-      <div className="authForm">
-        <h1>Sign In</h1>
-        <form onSubmit={registerUser}>
-          <div className="textbox">
-            <input
-              type="text"
-              placeholder="Username"
-              onChange={(e) => setRegisterUsername(e.target.value)}
-            />
-          </div>
-          <div className="textbox">
-            <input
-              type="password"
-              placeholder="password"
-              onChange={(e) => setRegisterPassword(e.target.value)}
-            />
-          </div>
-          <button className="btn">Register</button>
-          <p>
-            Already have an account ? <Link to="/logIn">Log In</Link>
-          </p>
-        </form>
-      </div>
-    </div>
-  );
+	return (
+		<div>
+			<ToastContainer />
+			<div className="authForm">
+				<h1>Sign In</h1>
+				<form onSubmit={registerUser}>
+					<div className="textbox">
+						<input
+							type="text"
+							placeholder="Username"
+							onChange={(e) => setRegisterUsername(e.target.value)}
+						/>
+					</div>
+					<div className="textbox">
+						<input
+							type="password"
+							placeholder="password"
+							onChange={(e) => setRegisterPassword(e.target.value)}
+						/>
+					</div>
+					<button className="btn">Register</button>
+					<p>
+						Already have an account ? <Link to="/logIn">Log In</Link>
+					</p>
+				</form>
+			</div>
+		</div>
+	);
 }
 
 export default Register;
--- controller/mailer.js
+++ controller/mailer.js
@@ -1,22 +1,39 @@
 const Transporter = require("../config/mailConfig");
-var QRCode = require('qrcode');
+var QRCode = require("qrcode");
+
+const json = [
+  {
+    "name": "John",
+    "reg": "RA001",
+    "email": "abc@gmail.com"
+  },
+  {
+    "name": "Doe",
+    "reg": "RA002",
+    "email": "xyz@gmail.com"
+  }
+];
+
+for( let a in json ){
+    console.log( json[a] );
+
 var img;
 
 let test = "";
 
-QRCode.toString('https://www.google.co.in/', function (err, url) {
-  console.log(url);
-  img = url;
+QRCode.toDataURL(json[a].reg, function (err, img)
+{
+  console.log(json[a].name);
+  let mailDetails = {
+    from: "yoman@helo.in",
+    to: json[a].email,
+    subject: "Test mail",
+    attachDataUrls: true,
+    html: `<span style='font-size: 2em;'>Hello</span><span style='color: #ec0101'>${json[a].name}</span><p>Thank You for choosing to attend our event on something. I would be an absolute honour to have you with us. Looking forward to seeing you there. Below you can find detils of your involvement in the event.</p><br><pre><img src="${img}"></pre>`,
+  };
+  (async () => {
+    let info = await Transporter.sendMail(mailDetails);
+    console.log("Message sent: %s", info.messageId);
+  })();
 });
-
-let mailDetails = {
-  from: "yoman@helo.in",
-  to: test,
-  subject: "Test mail",
-  html: "<span><h1 style='color:black'>Welcome</h1><h4>XYZ</h4></span><br><pre>"+ img +"</pre>",
 };
-
-(async () => {
-  let info = await Transporter.sendMail(mailDetails);
-  console.log("Message sent: %s", info.messageId);
-})();
