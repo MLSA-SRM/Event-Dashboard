2e74a01309495c5103955fd792c540a2724578fc
Kunal Singhal <kunal999936@gmail.com>
2020-09-14 13:35:40 +0530

    Prettier code format

--- client/src/App.js
+++ client/src/App.js
@@ -1,39 +1,44 @@
-import React, { useState } from 'react';
-import Register from './Register';
-import Login from './Login';
-import Home from './Home';
-import Table from './Table';
-import NewEvent from './NewEvent';
-import UserPage from './User';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import ProtectedRoute from './ProtectedRoute';
-import './App.css';
+import React, { useState } from "react";
+import Register from "./Register";
+import Login from "./Login";
+import Home from "./Home";
+import Table from "./Table";
+import NewEvent from "./NewEvent";
+import UserPage from "./User";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import ProtectedRoute from "./ProtectedRoute";
+import "./App.css";
 
 function App() {
-	const [ getusername, setUsername ] = useState('');
+  const [getusername, setUsername] = useState("");
 
-	return (
-		<Router>
-			<Switch>
-				<Route exact path="/">
-					<Login handleUsername={setUsername} />
-				</Route>
-				<Route exact path="/signIn">
-					<Register />
-				</Route>
-				<ProtectedRoute username={getusername} exact path="/dashboard" component={Home} />
-				<Route exact path="/newevent">
-					<NewEvent />
-				</Route>
-				<Route exact path="/user">
-					<UserPage />
-				</Route>
-				<Route exact path="/table">
-					<Table />
-				</Route>
-			</Switch>
-		</Router>
-	);
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <Login handleUsername={setUsername} />
+        </Route>
+        <Route exact path="/signIn">
+          <Register />
+        </Route>
+        <ProtectedRoute
+          username={getusername}
+          exact
+          path="/dashboard"
+          component={Home}
+        />
+        <Route exact path="/newevent">
+          <NewEvent />
+        </Route>
+        <Route exact path="/user">
+          <UserPage />
+        </Route>
+        <Route exact path="/table">
+          <Table />
+        </Route>
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
--- client/src/Table.js
+++ client/src/Table.js
@@ -1,79 +1,82 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import './Table.css';
-import { AgGridReact } from 'ag-grid-react';
-import 'ag-grid-community/dist/styles/ag-grid.css';
-import 'ag-grid-community/dist/styles/ag-theme-material.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import "./Table.css";
+import { AgGridReact } from "ag-grid-react";
+import "ag-grid-community/dist/styles/ag-grid.css";
+import "ag-grid-community/dist/styles/ag-theme-material.css";
 // import { log } from "debug";
 function Table() {
-	//   const columnDefs = [
-	//     {
-	//       headerName: "Name",
-	//       field: "name",
-	//       sortable: true,
-	//       checkboxSelection: true,
-	//       headerCheckboxSelection: true,
-	//     },
-	//     {
-	//       headerName: "Reg No",
-	//       field: "regNo",
-	//       sortable: true,
-	//     },
-	//     {
-	//       headerName: "Email",
-	//       field: "email",
-	//     },
-	//   ];
+  //   const columnDefs = [
+  //     {
+  //       headerName: "Name",
+  //       field: "name",
+  //       sortable: true,
+  //       checkboxSelection: true,
+  //       headerCheckboxSelection: true,
+  //     },
+  //     {
+  //       headerName: "Reg No",
+  //       field: "regNo",
+  //       sortable: true,
+  //     },
+  //     {
+  //       headerName: "Email",
+  //       field: "email",
+  //     },
+  //   ];
 
-	const [ rowDataValues, setRowDataValues ] = useState(null);
-	const [ column, setColumn ] = useState(null);
-	const [ dataparams, setDataParams ] = useState(null);
-	// const [mail, setMail] = useState(null);
-	useEffect(() => {
-		axios.get('/table').then((res) => {
-			let { header, data } = res.data;
-			//   console.log(res.data);
-			setColumn(header);
-			setRowDataValues(data);
-		});
-	}, []);
+  const [rowDataValues, setRowDataValues] = useState(null);
+  const [column, setColumn] = useState(null);
+  const [dataparams, setDataParams] = useState(null);
+  // const [mail, setMail] = useState(null);
+  useEffect(() => {
+    axios.get("/table").then((res) => {
+      let { header, data } = res.data;
+      //   console.log(res.data);
+      setColumn(header);
+      setRowDataValues(data);
+    });
+  }, []);
 
-	const sendMails = (e) => {
-		let data = [];
-		const selectedParticipants = dataparams.getSelectedNodes();
-		const selectData = selectedParticipants.map((node) => node.data);
-		// console.log(selectData);
-		selectData.forEach((item) => {
-			data.push({
-				name: item.name,
-				regno: item.reg,
-				email: item.email
-			});
-		});
-		// console.log(data);
-		axios.post('/mailer', { data }).then((res) => console.log(res.data)).catch((err) => console.log(err));
-	};
-	return (
-		<div>
-			<div
-				className="ag-theme-material list-data"
-				style={{
-					height: '700px',
-					width: '1000px'
-				}}
-			>
-				<button className="sendMailsButton" type="submit" onClick={sendMails}>
-					Send Email To Participants
-				</button>
-				<AgGridReact
-					animateRows
-					onGridReady={(params) => setDataParams(params.api)}
-					rowSelection="multiple"
-					columnDefs={column}
-					rowData={rowDataValues}
-				/>
-			</div>
-		</div>
-	);
+  const sendMails = (e) => {
+    let data = [];
+    const selectedParticipants = dataparams.getSelectedNodes();
+    const selectData = selectedParticipants.map((node) => node.data);
+    // console.log(selectData);
+    selectData.forEach((item) => {
+      data.push({
+        name: item.name,
+        regno: item.reg,
+        email: item.email,
+      });
+    });
+    // console.log(data);
+    axios
+      .post("/mailer", { data })
+      .then((res) => console.log(res.data))
+      .catch((err) => console.log(err));
+  };
+  return (
+    <div>
+      <div
+        className="ag-theme-material list-data"
+        style={{
+          height: "700px",
+          width: "1000px",
+        }}
+      >
+        <button className="sendMailsButton" type="submit" onClick={sendMails}>
+          Send Email To Participants
+        </button>
+        <AgGridReact
+          animateRows
+          onGridReady={(params) => setDataParams(params.api)}
+          rowSelection="multiple"
+          columnDefs={column}
+          rowData={rowDataValues}
+        />
+      </div>
+    </div>
+  );
 }
 export default Table;
