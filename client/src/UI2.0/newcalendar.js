afee162265ef66d9baf6430bb85b7da5b6e5be14
jugal09xx <65349424+jugal09xx@users.noreply.github.com>
2020-09-22 12:20:39 +0530

    add event calendar

--- client/src/UI2.0/newcalendar.js
+++ client/src/UI2.0/newcalendar.js
@@ -1,13 +1,35 @@
-import React from 'react';
-import NewNav from './newnav';
+import React from "react";
+import FullCalendar from "@fullcalendar/react";
+import dayGridPlugin from "@fullcalendar/daygrid";
+import NewNav from "./newnav";
+import {ToastContainer, toast} from 'react-toastify';
 
-const NewCalendar = () => {
-    return(
-        <div>
-            <NewNav/>
-            <h1>Calendar will go here!</h1>
+//ADD EVENT DATA HERE
+const events = [
+  { title: "event 1", date: "2020-09-23" },
+  { title: "event2", date: "2020-09-24" },
+];
+
+export default class NewCalendar extends React.Component {
+  render() {
+    return (
+      <div>
+        <NewNav />
+        <div style={{ width: "80%", margin: "auto", marginTop: "10vh" }}>
+          <FullCalendar
+            plugins={[dayGridPlugin]}
+            eventClick={notify}
+            initialview="dayGridMonth"
+            events={events}
+            height={500}
+          />
+          <ToastContainer position={"bottom-right"}
+          />
         </div>
+      </div>
     );
+  }
 }
 
-export default NewCalendar;
+const notify = (info) => toast.info("The event you are viewing is " +info.event.title);
+
