4b58ef603e8061f80c862c66bee8b4fa0be3b20c
Aryan <51026686+Violent-Idiot@users.noreply.github.com>
2020-09-23 10:14:41 +0530

    yo

--- app.js
+++ app.js
@@ -5,8 +5,9 @@ var passport = require("passport");
 var logger = require("morgan");
 var indexRouter = require("./routes/index");
 var session = require("express-session");
+var MongoStore = require("connect-mongo")(session);
 require("./config/passportConfig")(passport);
-// var mongoose = require("mongoose");
+var mongoose = require("mongoose");
 // var User = require("./models/user");
 
 var app = express();
@@ -26,8 +27,15 @@ app.use(cookieParser());
 app.use(
   session({
     secret: "secret",
-    resave: false,
-    saveUninitialized: false,
+    resave: true,
+    rolling: true,
+    saveUninitialized: true,
+    cookie: {
+      expires: 1 * 60 * 60 * 1000,
+    },
+    store: new MongoStore({
+      mongooseConnection: mongoose.connection,
+    }),
   })
 );
 
