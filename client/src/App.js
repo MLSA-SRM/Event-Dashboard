18f379dd044a897eba2590ce0c8b237dea70f24b
jugal09xx <65349424+jugal09xx@users.noreply.github.com>
2020-09-30 00:37:29 +0530

    revamp user page

--- client/src/App.js
+++ client/src/App.js
@@ -11,6 +11,7 @@ import NewDash from "./UI2.0/newdashboard";
 import NewPeople from "./UI2.0/newpeople";
 import AddEvent from "./UI2.0/addevent";
 import NewCalendar from "./UI2.0/newcalendar";
+import Profile from './UI2.0/newprofile';
 // import usePersist from "./components/Persist";
 import {
   BrowserRouter as Router,
@@ -77,6 +78,9 @@ function App(props) {
           </Route> */}
         <ProtectedRoute exact path="/newpeople">
           <NewPeople />
+        </ProtectedRoute>
+        <ProtectedRoute exact path="/abc">
+          <Profile />
         </ProtectedRoute>
         <ProtectedRoute exact path="/newcalendar">
           <NewCalendar />
