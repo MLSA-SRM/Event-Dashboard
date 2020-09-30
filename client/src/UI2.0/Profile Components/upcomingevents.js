837483b0a251c055199a8a338fade2123c1dc36e
jugal09xx <65349424+jugal09xx@users.noreply.github.com>
2020-09-30 11:02:26 +0530

    routing profile componenets

---
+++ client/src/UI2.0/Profile Components/upcomingevents.js
@@ -0,0 +1,67 @@
+import React from 'react';
+
+const UpcomingEvents = () => {
+    return(
+        <div className="events-lists">
+          <h4>UPCOMING EVENTS</h4>
+          <div className="events-row">
+            <div className="events-box">
+              <h4>Event Name</h4>
+              <h6>11:00 AM</h6>
+              <h6>Thursday</h6>
+              <h6>SRM University</h6>
+            </div>
+            <div className="events-box">
+              <h4>Event Name</h4>
+              <h6>11:00 AM</h6>
+              <h6>Thursday</h6>
+              <h6>SRM University</h6>
+            </div>
+          </div>
+          <div className="events-row">
+            <div className="events-box">
+              <h4>Event Name</h4>
+              <h6>11:00 AM</h6>
+              <h6>Thursday</h6>
+              <h6>SRM University</h6>
+            </div>
+            <div className="events-box">
+              <h4>Event Name</h4>
+              <h6>11:00 AM</h6>
+              <h6>Thursday</h6>
+              <h6>SRM University</h6>
+            </div>
+          </div>
+          <div className="events-row">
+            <div className="events-box">
+              <h4>Event Name</h4>
+              <h6>11:00 AM</h6>
+              <h6>Thursday</h6>
+              <h6>SRM University</h6>
+            </div>
+            <div className="events-box">
+              <h4>Event Name</h4>
+              <h6>11:00 AM</h6>
+              <h6>Thursday</h6>
+              <h6>SRM University</h6>
+            </div>
+          </div>
+          <div className="events-row">
+            <div className="events-box">
+              <h4>Event Name</h4>
+              <h6>11:00 AM</h6>
+              <h6>Thursday</h6>
+              <h6>SRM University</h6>
+            </div>
+            <div className="events-box">
+              <h4>Event Name</h4>
+              <h6>11:00 AM</h6>
+              <h6>Thursday</h6>
+              <h6>SRM University</h6>
+            </div>
+          </div>
+        </div>
+    );
+}
+
+export default UpcomingEvents;
