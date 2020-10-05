6154973a3fea8764c8146f5adf2f061a2113a56b
Aryan <51026686+Violent-Idiot@users.noreply.github.com>
2020-10-05 23:38:39 +0530

    added mailer scheduler to mail template

--- client/src/UI2.0/newpeople.js
+++ client/src/UI2.0/newpeople.js
@@ -2,17 +2,17 @@ import React from "react";
 import Table from "../Table";
 import NewNav from "./newnav";
 import Box from "../textbox";
-import MailScheduler from "./MailScheduler";
+// import MailScheduler from "./MailScheduler";
 const NewPeople = () => {
   return (
-    <div className="margin">
+    <div className='margin'>
       <NewNav />
       <br />
       <br />
-      <h1 className="heading">Send Mail</h1>
+      <h1 className='heading'>Send Mail</h1>
       <br />
       <Box />
-      <MailScheduler />
+      {/* <MailScheduler /> */}
       <Table style={{ width: "100%" }} />
     </div>
   );
